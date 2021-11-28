69fa52cbb3c2ff620b3e132cc3ce6809393450d1
Okamochi000 <okkanw@yahoo.co.jp>
2021-11-28 21:36:34 +0900

    プロジェクト追加

---
+++ Project/Assets/WindowGroup/Scripts/OpenAndCloseBase.cs
@@ -0,0 +1,218 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 開閉ベース
+/// </summary>
+public class OpenAndCloseBase : MonoBehaviour
+{
+    /// <summary>
+    /// 開閉状態
+    /// </summary>
+    public enum OpenState
+    {
+        OpenAnim,
+        Opened,
+        CloseAnim,
+        Closed
+    }
+
+    /// <summary>
+    /// 開閉アニメーションの種類
+    /// </summary>
+    protected enum OpenAnimType
+    {
+        None,
+        Animator,
+        Script
+    }
+
+    /// <summary>
+    /// 開閉操作の種類
+    /// </summary>
+    private enum OpenActionType
+    {
+        None,
+        Open,
+        Close
+    }
+
+    public OpenState CurrentOpenState { get; private set; } = OpenState.Closed;
+
+    [SerializeField] protected OpenAnimType openAnimType = OpenAnimType.None;
+    [SerializeField] protected bool isAutoActive = true;
+
+    private OpenActionType nextOpenAction_ = OpenActionType.None;
+
+    public virtual void OnEnable()
+    {
+        // Openを使用しないときの自動開閉
+        if (!isAutoActive) { return; }
+        if (!this.gameObject.activeSelf || CurrentOpenState == OpenState.Closed) { return; }
+        Open();
+    }
+
+    public virtual void OnDisable()
+    {
+        // Closeを使用しないときの自動開閉
+        if (isAutoActive)
+        {
+            if (!this.gameObject.activeSelf) { Close(); }
+        }
+
+        // Animatorが指定されていた場合アニメーションを終了
+        if (openAnimType == OpenAnimType.Animator)
+        {
+            if (CurrentOpenState == OpenState.OpenAnim) { ChangeOpenState(OpenState.Opened); }
+            else if (CurrentOpenState == OpenState.CloseAnim) { ChangeOpenState(OpenState.Closed); }
+        }
+    }
+
+    /// <summary>
+    /// 開く
+    /// </summary>
+    public void Open()
+    {
+        // 閉じ切っていない場合は閉じ切るまで待つ
+        if (CurrentOpenState != OpenState.Closed)
+        {
+            nextOpenAction_ = OpenActionType.Open;
+            return;
+        }
+
+        // 開閉アニメーション更新
+        nextOpenAction_ = OpenActionType.None;
+        ChangeOpenState(OpenState.OpenAnim, true);
+
+        // 開ききっている、もしくはスクリプト準拠の場合は終了
+        if (CurrentOpenState == OpenState.Opened) { return; }
+        if (openAnimType == OpenAnimType.Script) { return; }
+
+        // アニメーションの終了チェック
+        bool isFinishedAnim = true;
+        if (openAnimType == OpenAnimType.Animator)
+        {
+            if (this.gameObject.activeInHierarchy)
+            {
+                // アクティブ状態ならアニメーショントリガー設定
+                Animator animator = this.GetComponent<Animator>();
+                if (animator != null && animator.runtimeAnimatorController != null)
+                {
+                    animator.SetTrigger("Open");
+                    isFinishedAnim = false;
+                }
+            }
+        }
+        if (isFinishedAnim) { ChangeOpenState(OpenState.Opened, true); }
+    }
+
+    /// <summary>
+    /// 閉じる
+    /// </summary>
+    public void Close()
+    {
+        // 開ききっていない場合は開ききるまで待つ
+        if (CurrentOpenState != OpenState.Opened)
+        {
+            nextOpenAction_ = OpenActionType.Close;
+            return;
+        }
+
+        // 開閉アニメーション更新
+        nextOpenAction_ = OpenActionType.None;
+        ChangeOpenState(OpenState.CloseAnim, true);
+
+        // 閉じ切っている、もしくはスクリプト準拠の場合は終了
+        if (CurrentOpenState == OpenState.Closed) { return; }
+        if (openAnimType == OpenAnimType.Script) { return; }
+
+        // アニメーションの終了チェック
+        bool isFinishedAnim = true;
+        if (openAnimType == OpenAnimType.Animator)
+        {
+            if (this.gameObject.activeInHierarchy)
+            {
+                // アクティブ状態ならアニメーショントリガー設定
+                Animator animator = this.GetComponent<Animator>();
+                if (animator != null && animator.runtimeAnimatorController != null)
+                {
+                    animator.SetTrigger("Close");
+                    isFinishedAnim = false;
+                }
+            }
+        }
+        if (isFinishedAnim) { ChangeOpenState(OpenState.Closed, true); }
+    }
+
+    /// <summary>
+    /// 開閉状態を取得する
+    /// </summary>
+    /// <returns></returns>
+    public bool IsOpen()
+    {
+        return (CurrentOpenState != OpenState.Closed);
+    }
+
+    /// <summary>
+    /// 開閉状態切替
+    /// </summary>
+    /// <param name="state"></param>
+    /// <param name="isCallback"></param>
+    protected void ChangeOpenState(OpenState state, bool isCallback = true)
+    {
+        // ステータスが同じなら何もしない
+        if (CurrentOpenState == state) { return; }
+
+        // 開閉状態切替
+        CurrentOpenState = state;
+
+        // 表示状態切替
+        if (isAutoActive && Application.isPlaying)
+        {
+            if (CurrentOpenState == OpenState.OpenAnim) { this.gameObject.SetActive(true); }
+            else if (CurrentOpenState == OpenState.Closed) { this.gameObject.SetActive(false); }
+        }
+
+        // コールバック呼び出し
+        if (isCallback) { OnChangedOpenState(state); }
+
+        // 次の操作呼び出し
+        if (CurrentOpenState == OpenState.Opened)
+        {
+
+            if (nextOpenAction_ == OpenActionType.Close) { Close(); }
+            nextOpenAction_ = OpenActionType.None;
+        }
+        else if (CurrentOpenState == OpenState.Closed)
+        {
+            if (nextOpenAction_ == OpenActionType.Open) { Open(); }
+            nextOpenAction_ = OpenActionType.None;
+        }
+    }
+
+    /// <summary>
+    /// 開閉状態変更コールバック
+    /// </summary>
+    /// <param name="state"></param>
+    protected virtual void OnChangedOpenState(OpenState state) { }
+
+    /// <summary>
+    /// アニメーショントリガー
+    /// </summary>
+    /// <param name="eventName"></param>
+    private void OnAnimationTrigger(string eventName)
+    {
+        if (openAnimType != OpenAnimType.Animator) { return; }
+
+        // トリガーリセット
+        Animator animator = this.GetComponent<Animator>();
+        if (animator != null && animator.runtimeAnimatorController != null)
+        {
+            animator.ResetTrigger("Open");
+            animator.ResetTrigger("Close");
+        }
+
+        // 遷移
+        if (eventName == "Open") { ChangeOpenState(OpenState.Opened, true); }
+        else if (eventName == "Close") { ChangeOpenState(OpenState.Closed, true); }
+    }
+}
---
+++ Project/Assets/WindowGroup/Scripts/WindowGroup.cs
@@ -0,0 +1,310 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ウィンドウ切替管理
+/// </summary>
+public class WindowGroup : MonoBehaviour
+{
+    /// <summary>
+    /// 切替方法
+    /// </summary>
+    public enum TransitionType
+    {
+        Single,         // 切替を順々に行う
+        CrossDissolve,  // 切替を同時に行う
+    }
+
+    /// <summary>
+    /// 開閉状態
+    /// </summary>
+    public enum OpenState
+    {
+        Closed,     // 閉じている
+        Transtion,  // 遷移中
+        Opened      // 開いている
+    }
+
+    public OpenState CurrentOpenState { get; private set; } = OpenState.Closed;
+
+    [SerializeField] private List<OpenAndCloseBase> windowList = new List<OpenAndCloseBase>();
+    [SerializeField] private TransitionType transitionType = TransitionType.CrossDissolve;
+    [SerializeField] private OpenAndCloseBase background = null;
+    [SerializeField] private GameObject frontClickBlocker = null;
+
+    private List<int> windowHistory_ = new List<int>();
+    private int activeIndex_ = -1;
+
+    /// <summary>
+    /// ウィンドウを開く
+    /// </summary>
+    /// <param name="windowNumber"></param>
+    public void Open(int windowNumber)
+    {
+        // 同じウィンドウの場合は何もしない
+        if (windowNumber == GetActiveWindowNumber()) { return; }
+
+        // 遷移中は何もしない
+        if (CurrentOpenState == OpenState.Transtion) { return; }
+
+        // ウィンドウが存在しない場合は何もしない
+        if (GetWindow(windowNumber) == null) { return; }
+
+        // 開閉履歴調整
+        if (activeIndex_ <= (windowHistory_.Count - 1))
+        {
+            int tempIndex = (activeIndex_ + 1);
+            while (tempIndex < windowHistory_.Count) { windowHistory_.RemoveAt((windowHistory_.Count - 1)); }
+        }
+        windowHistory_.Add(windowNumber);
+
+        // 遷移開始
+        Transition(true);
+    }
+
+    /// <summary>
+    /// 次に進む
+    /// </summary>
+    public void Next()
+    {
+        // 遷移中は何もしない
+        if (CurrentOpenState == OpenState.Transtion) { return; }
+
+        // 次のウィンドウがない場合は何もしない
+        if (activeIndex_ >= (windowHistory_.Count -1)) { return; }
+
+        // 遷移開始
+        Transition(true);
+    }
+
+    /// <summary>
+    /// 1つ前に戻る
+    /// </summary>
+    public void Back()
+    {
+        // 遷移中は何もしない
+        if (CurrentOpenState == OpenState.Transtion) { return; }
+
+        // 前のウィンドウがない場合は何もしない
+        if (activeIndex_ < 1) { CloseAll(); }
+        else { Transition(false); }
+    }
+
+    /// <summary>
+    /// すべて閉じる
+    /// </summary>
+    public void CloseAll()
+    {
+        // 遷移中は何もしない
+        if (CurrentOpenState == OpenState.Transtion) { return; }
+
+        // 初期化
+        windowHistory_.Clear();
+        activeIndex_ = -1;
+
+        // 遷移開始
+        StartCoroutine(TransitionCloseAll());
+    }
+
+    /// <summary>
+    /// アクティブなウィンドウ番号を取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetActiveWindowNumber()
+    {
+        if (activeIndex_ >= windowHistory_.Count || activeIndex_ < 0) { return -1; }
+        return windowHistory_[activeIndex_];
+    }
+
+    /// <summary>
+    /// ウィンドウを取得する
+    /// </summary>
+    /// <param name="windowNumber"></param>
+    /// <returns></returns>
+    public OpenAndCloseBase GetWindow(int windowNumber)
+    {
+        if (windowNumber >= windowList.Count || windowNumber < 0) { return null; }
+        return windowList[windowNumber];
+    }
+
+    /// <summary>
+    /// 現在アクティブなウィンドウを取得する
+    /// </summary>
+    /// <param name="windowNumber"></param>
+    /// <returns></returns>
+    public OpenAndCloseBase GetActiveWindow()
+    {
+        return GetWindow(GetActiveWindowNumber());
+    }
+
+    /// <summary>
+    /// 全ウィンドウが閉じた
+    /// </summary>
+    protected virtual void OnClosedAllWindow() { }
+
+    /// <summary>
+    /// 遷移
+    /// </summary>
+    /// <param name="isNext"></param>
+    private void Transition(bool isNext)
+    {
+        switch (transitionType)
+        {
+            case TransitionType.Single: StartCoroutine(TransitionSingle(isNext)); break;
+            case TransitionType.CrossDissolve: StartCoroutine(TransitionCrossDissolve(isNext)); break;
+            default: break;
+        }
+    }
+
+    /// <summary>
+    /// 全てのウィンドウを閉じる
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TransitionCloseAll()
+    {
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Transtion;
+
+        // 背景を開く
+        if (background != null) { background.Close(); }
+
+        // 前面クリックブロッカーを表示する
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(true); }
+
+        // 現在のウィンドウを閉じる
+        List<OpenAndCloseBase> activeWindowList = new List<OpenAndCloseBase>();
+        foreach (OpenAndCloseBase window in windowList)
+        {
+            if (window != null && window.IsOpen())
+            {
+                window.Close();
+                activeWindowList.Add(window);
+            }
+        }
+
+        // 全てのウィンドウが閉じるまで待つ
+        bool isAllClosed = (activeWindowList.Count == 0);
+        while (!isAllClosed)
+        {
+            isAllClosed = true;
+            foreach (OpenAndCloseBase window in activeWindowList)
+            {
+                if (window.IsOpen())
+                {
+                    isAllClosed = false;
+                    break;
+                }
+            }
+
+            yield return null;
+        }
+
+        // 背景が閉じ切るまで待つ
+        if (background != null)
+        {
+            while (background.IsOpen()) { yield return null; }
+        }
+
+        // 前面クリックブロッカーを非表示にする
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(false); }
+
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Closed;
+
+        // コールバック呼び出し
+        OnClosedAllWindow();
+    }
+
+    /// <summary>
+    /// 順々にウィンドウを切り替える
+    /// </summary>
+    /// <param name="isNext"></param>
+    /// <returns></returns>
+    private IEnumerator TransitionSingle(bool isNext)
+    {
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Transtion;
+
+        // 背景を開く
+        if (background != null) { background.Open(); }
+
+        // 前面クリックブロッカーを表示する
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(true); }
+
+        // 現在のウィンドウを閉じる
+        OpenAndCloseBase activeWindow = GetActiveWindow();
+        if (activeWindow != null)
+        {
+            activeWindow.Close();
+            while (activeWindow.CurrentOpenState != OpenAndCloseBase.OpenState.Closed) { yield return null; }
+        }
+
+        // 次のウィンドウが開かれるまで待つ
+        if (isNext) { activeIndex_++; }
+        else { activeIndex_--; }
+        OpenAndCloseBase nextWindow = GetActiveWindow();
+        nextWindow.Open();
+        while (nextWindow.CurrentOpenState != OpenAndCloseBase.OpenState.Opened) { yield return null; }
+
+        // 背景が開ききるまで待つ
+        if (background != null)
+        {
+            while (background.CurrentOpenState != OpenAndCloseBase.OpenState.Opened) { yield return null; }
+        }
+
+        // 前面クリックブロッカーを非表示にする
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(false); }
+
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Opened;
+    }
+
+    /// <summary>
+    /// 同時にウィンドウを切り替える
+    /// </summary>
+    /// <param name="isNext"></param>
+    /// <returns></returns>
+    private IEnumerator TransitionCrossDissolve(bool isNext)
+    {
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Transtion;
+
+        // 背景を開く
+        if (background != null) { background.Open(); }
+
+        // 前面クリックブロッカーを表示する
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(true); }
+
+        // 現在のウィンドウを閉じる
+        OpenAndCloseBase activeWindow = GetActiveWindow();
+        if (activeWindow != null) { activeWindow.Close(); }
+
+        // 次のウィンドウが開かれるまで待つ
+        if (isNext) { activeIndex_++; }
+        else { activeIndex_--; }
+        OpenAndCloseBase nextWindow = GetActiveWindow();
+        nextWindow.Open();
+
+        // 現在のウィンドウが閉じ切るまで待つ
+        if (activeWindow != null)
+        {
+            while (activeWindow.CurrentOpenState != OpenAndCloseBase.OpenState.Closed) { yield return null; }
+        }
+
+        // 次のウィンドウが開ききるまで待つ
+        while (nextWindow.CurrentOpenState != OpenAndCloseBase.OpenState.Opened) { yield return null; }
+
+        // 背景が開ききるまで待つ
+        if (background != null)
+        {
+            while (background.CurrentOpenState != OpenAndCloseBase.OpenState.Opened) { yield return null; }
+        }
+
+        // 前面クリックブロッカーを非表示にする
+        if (frontClickBlocker != null) { frontClickBlocker.SetActive(false); }
+
+        // 開閉状態変更
+        CurrentOpenState = OpenState.Opened;
+    }
+}
